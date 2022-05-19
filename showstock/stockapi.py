3164b49b67dc95ef6f5a5a883e4e47dfa97111b8
Prashant Joshi <joshiprashanthd121@gmail.com>
2022-05-19 21:01:42 +0530

    fix: use Stock instead of StockData

--- showstock/stockapi.py
+++ showstock/stockapi.py
@@ -1,18 +1,17 @@
 import requests
-from showstock.stockdata import StockData
+from showstock.stock import Stock
 
-def get_stock_data(symbol: str) -> StockData:
+
+def get_stock_data(symbol: str) -> Stock:
     url = "https://query1.finance.yahoo.com/v7/finance/quote?symbols=" + symbol
-    response = requests.get(url, headers={
-        'User-Agent': ''
-    })
+    response = requests.get(url, headers={"User-Agent": ""})
     data = response.json()
-    data =  data['quoteResponse']['result'][0] 
-    return StockData(
-        marketState=data['marketState'],
-        price=data['regularMarketPrice'],
-        symbol=data['symbol'],
-        marketChange=data['regularMarketChange'],
-        marketChangePercent=data['regularMarketChangePercent'],
-        currency=data['currency']
+    data = data["quoteResponse"]["result"][0]
+    return Stock(
+        marketState=data["marketState"],
+        price=data["regularMarketPrice"],
+        symbol=data["symbol"],
+        marketChange=data["regularMarketChange"],
+        marketChangePercent=data["regularMarketChangePercent"],
+        currency=data["currency"],
     )
