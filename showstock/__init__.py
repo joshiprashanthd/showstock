d3d7ac091a947827bebad2ab6748fb103ad60cdc
Prashant Joshi <joshiprashanthd121@gmail.com>
2022-05-19 16:23:16 +0530

    chore: define codes for errors

--- showstock/__init__.py
+++ showstock/__init__.py
@@ -1,3 +1,19 @@
 __appname__ = "showstock"
 __version__ = "0.0.1"
 __author__ = "Prashant Joshi"
+
+(
+    SUCCESS,
+    DIR_ERROR,
+    FILE_ERROR,
+    DB_READ_ERROR,
+    DB_WRITE_ERROR,
+    JSON_ERROR,
+) = range(6)
+
+ERRORS = {
+    DIR_ERROR: "config directory error",
+    FILE_ERROR: "config file error",
+    DB_READ_ERROR: "database read error",
+    DB_WRITE_ERROR: "database write error",
+}
