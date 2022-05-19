642f79ff411502f350b23eb76e76c51d8705bf2e
Prashant Joshi <joshiprashanthd121@gmail.com>
2022-05-19 15:24:15 +0530

    chore: add configuration
    create setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+from showstock import __appname__, __version__, __author__
+
+setup(
+    name=__appname__,
+    version=__version__,
+    author=__author__,
+    packages=["showstock"],
+    requires=["requests", "typer", "rich"],
+    entry_points={"console_scripts": ["showstock = showstock.main:main"]} ,
+)
