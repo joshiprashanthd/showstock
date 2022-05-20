45bd88a99f4f2887256526f08288e3ea87338f6e
Prashant Joshi <joshiprashanthd121@gmail.com>
2022-05-20 20:09:27 +0530

    fix: catch connection errors and print appropriate message

--- showstock/stockapi.py
+++ showstock/stockapi.py
@@ -1,10 +1,15 @@
 import requests
 from showstock.stock import Stock
+import typer
 
 
 def get_stock_data(symbol: str) -> Stock:
     url = "https://query1.finance.yahoo.com/v7/finance/quote?symbols=" + symbol
-    response = requests.get(url, headers={"User-Agent": ""})
+    try:
+        response = requests.get(url, headers={"User-Agent": ""})
+    except:
+        typer.secho("Cannot connect to the Internet!", fg=typer.colors.RED)
+        raise typer.Exit(1)
     data = response.json()
     data = data["quoteResponse"]["result"][0]
     return Stock(
